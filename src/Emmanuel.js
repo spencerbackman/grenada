92c16c5a69464809da919984f249a66214d331d7
spencerbackman <backmanspencer99@gmail.com>
2020-05-08 15:15:01 -0500

    updated church services

--- src/Emmanuel.js
+++ src/Emmanuel.js
@@ -9,8 +9,12 @@ export default function Emmanuel() {
         <div className="tv-page-wrapper">
             <div className="tv-top-container">
                 <h1 className="tv-header"> Emmanuel </h1>
-                <iframe title="Emmanuel Service" src="https://player.vimeo.com/video/414058017" frameBorder="0" allowFullScreen></iframe>
+                <iframe title="Emmanuel Service" src="https://player.vimeo.com/video/416459192" frameBorder="0" allowFullScreen></iframe>
             </div>
+            <div className="tv-mid-container">
+                <h2 className="tv-header2"> Past Services </h2>
+                <a className="tv-video-link" href="https://vimeo.com/414058017"> 4-26-20 Service </a>
+            </div> 
         </div>
     )
 }
--- src/FirstBaptistChurch.js
+++ src/FirstBaptistChurch.js
@@ -11,13 +11,15 @@ export default function FirstBaptistChurch() {
                 <h1 className="tv-header"> First Baptist Church </h1>
                 <iframe title="First Baptist Church Service" src="https://player.vimeo.com/video/415151639" frameBorder="0" allowFullScreen></iframe>
                 <h2 className="tv-header2"> PM Service </h2>
-                <iframe title="First Baptist Church PM Service" src="https://player.vimeo.com/video/414066183" frameBorder="0" allowFullScreen></iframe>
+                <iframe title="First Baptist Church PM Service" src="https://player.vimeo.com/video/416451438" frameBorder="0" allowFullScreen></iframe>
                 <h2 className="tv-header2"> Sunday School </h2>
-                <iframe title="First Baptist Church Sunday School" src="https://player.vimeo.com/video/414052256" frameBorder="0" allowFullScreen></iframe>
+                <iframe title="First Baptist Church Sunday School" src="https://player.vimeo.com/video/416461569" frameBorder="0" allowFullScreen></iframe>
             </div>
             <div className="tv-mid-container">
                 <h2 className="tv-header2"> Past Services </h2>
                 <a className="tv-video-link" href="https://vimeo.com/414066584"> 4-26-20 AM Service </a>
+                <a className="tv-video-link" href="https://vimeo.com/414066183"> 4-26-20 PM Service </a>
+                <a className="tv-video-link" href="https://vimeo.com/414052256"> 5-3-20 Sunday School </a>
             </div>
         </div>
     )
